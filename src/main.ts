49f69b9b8b0b75efabb4025290b7736be2786169
Ali Heidari <ali-heidari@outlook.com>
2022-07-19 00:50:19 +0430

    Modify global routing to api/v1/...

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+  app.setGlobalPrefix(`api/v${version.split('.')[0]}`)
   await app.listen(3000);
 }
 bootstrap();
