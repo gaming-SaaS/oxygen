60528ac84699f9ff194dfa7fc020a8aa3e6a2e7f
Ali Heidari <ali-heidari@outlook.com>
2022-07-22 04:54:16 +0430

    Add name to bootstrap function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
-// import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [ UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
